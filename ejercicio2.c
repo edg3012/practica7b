e0fbf280a7260aee2a97f24bde7bac6e28916bd8
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-09-27 14:12:31 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,30 @@
+#include <stadio.h>
+int main ()
+{
+  int uno, dos, tres;
+  printf(“ingrese tres numeros separados por espacios:\n");
+    scanf  ("%d %d %d , &uno, &dos, &tres);
+    if  (uno> dos)
+    {
+      if  (dos >tres)
+      {
+        printf("%d es mayor a %d\n", uno, dos, tres);
+      }
+      else
+          if (uno> tres
+          {
+              printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);
+          }
+          else
+          {
+              printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);
+          }
+      }
+}
+else
+{
+    if(dos > tres)
+       if (tres > uno)
+           printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
+
+    
